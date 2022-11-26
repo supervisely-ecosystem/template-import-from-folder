efe64d06554cafc3831a56bc356a124596936d41
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-26 15:48:47 +0000

    need tests in production setting

--- src/main.py
+++ src/main.py
@@ -12,14 +12,19 @@ load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 
 class MyImport(sly.app.Import):
-    def process_file(self, workspace_id: int, path: str):
-        name = Path(path).stem
+    def process(self, workspace_id: int, path: str, is_directory: bool):
+        # create api object to communicate with Supervisely Server
         api = sly.Api.from_env()
-        # project = api.project.create(workspace_id, name, change_name_if_conflict=True)
-        # print(f"Project has been successfully created: id={project.id}, name={project.name}")
-        # dataset = api.dataset.create(project.id, "dataset-01")
-        # print(f"Dataset has been successfully created: id={dataset.id}, name={dataset.name}")
 
+        # create project with the same name as input file
+        project_name = Path(path).stem
+        project = api.project.get_or_create(workspace_id, project_name)
+        print(f"Working project: id={project.id}, name={project.name}")
+
+        dataset = api.dataset.create(project.id, "dataset", change_name_if_conflict=True)
+        print(f"Created dataset: id={dataset.id}, name={dataset.name}")
+
+        names = set()
         with open(path) as file:
             for line in file:
                 image_url = line.strip()
@@ -30,9 +35,28 @@ class MyImport(sly.app.Import):
                 # get image name from response
                 d = response.headers["content-disposition"]
                 img_name = re.findall("filename=(.+)", d)[0]
+                img_path = os.path.join("data", img_name)
+
+                # save image
                 img_data = response.content
-                with open(img_name, "wb") as file:
+                with open(img_path, "wb") as file:
                     file.write(img_data)
+
+                # check if image with the same name already exists in our new dataset
+                if img_name in names:
+                    img_name = sly.generate_free_name(
+                        used_names=names, possible_name=img_name, with_ext=True
+                    )
+
+                # upload image into dataset on Supervisely server
+                info = api.image.upload_path(dataset.id, img_name, img_path)
+                sly.logger.trace(f"Image has been uploaded: id={info.id}, name={info.name}")
+                names.add(img_name)
+
+                # remove local file after upload
+                os.remove(img_path)
+
+        return project.id
 
 
 app = MyImport()
