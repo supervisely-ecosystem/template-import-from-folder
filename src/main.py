73dea30eff389e5189436b87bb832eba04e7e104
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-26 15:59:44 +0000

    need tests

--- src/main.py
+++ src/main.py
@@ -26,35 +26,48 @@ class MyImport(sly.app.Import):
 
         names = set()
         with open(path) as file:
+            # process text file and remove empty lines
+            lines = []
             for line in file:
-                image_url = line.strip()
+                normalized_line = line.strip()
+                if normalized_line != "":
+                    lines.append(normalized_line)
 
-                # download image
-                response = requests.get(image_url)
+            progress = sly.Progress("Processing urls", total_cnt=len(lines))
+            for image_url in lines:
+                try:
+                    # download image
+                    response = requests.get(image_url)
 
-                # get image name from response
-                d = response.headers["content-disposition"]
-                img_name = re.findall("filename=(.+)", d)[0]
-                img_path = os.path.join("data", img_name)
+                    # get image name from response
+                    d = response.headers["content-disposition"]
+                    img_name = re.findall("filename=(.+)", d)[0]
+                    img_path = os.path.join("data", img_name)
 
-                # save image
-                img_data = response.content
-                with open(img_path, "wb") as file:
-                    file.write(img_data)
+                    # save image
+                    img_data = response.content
+                    with open(img_path, "wb") as file:
+                        file.write(img_data)
 
-                # check if image with the same name already exists in our new dataset
-                if img_name in names:
-                    img_name = sly.generate_free_name(
-                        used_names=names, possible_name=img_name, with_ext=True
+                    # check if image with the same name already exists in our new dataset
+                    if img_name in names:
+                        img_name = sly.generate_free_name(
+                            used_names=names, possible_name=img_name, with_ext=True
+                        )
+
+                    # upload image into dataset on Supervisely server
+                    info = api.image.upload_path(dataset.id, img_name, img_path)
+                    sly.logger.trace(f"Image has been uploaded: id={info.id}, name={info.name}")
+                    names.add(img_name)
+
+                    # remove local file after upload
+                    os.remove(img_path)
+                except Exception as e:
+                    sly.logger.warn(
+                        "Can not process url, skipped", extra={"url": image_url, "reason": repr(e)}
                     )
-
-                # upload image into dataset on Supervisely server
-                info = api.image.upload_path(dataset.id, img_name, img_path)
-                sly.logger.trace(f"Image has been uploaded: id={info.id}, name={info.name}")
-                names.add(img_name)
-
-                # remove local file after upload
-                os.remove(img_path)
+                finally:
+                    progress.iter_done_report()
 
         return project.id
 
